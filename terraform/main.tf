ca86c26b877d6a62e229fb7e3e322fe076a547f1
DMoises <3033997670108@ingenieria.usac.edu.gt>
2026-06-07 18:14:46 -0600

    feat: usar credenciales de gcp

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "google" {
-  credentials = file(var.credentials_path)
+  # credentials = file(var.credentials_path)
   project     = var.gcp_project_id
   region      = var.gcp_region
 }

2b2fe1b05e57816c136f2de41c4a133a95981c31
Juanjo264 <juanjoalmengor100@gmail.com>
2026-06-08 00:56:26 -0600

    feat: servicio de Planes y Suscripciones

---
+++ backend/billing-service/src/database/db.ts
@@ -0,0 +1,13 @@
+import { config } from 'dotenv';
+import { join } from 'path';
+import { Pool } from 'pg';
+
+config({ path: join(process.cwd(), '../../.env') });
+
+export const db = new Pool({
+  host: 'localhost',
+  port: Number(process.env.SUBSCRIPTION_DB_PORT || 5434),
+  user: process.env.SUBSCRIPTION_DB_USER || 'subscription',
+  password: process.env.SUBSCRIPTION_DB_PASSWORD || 'admin',
+  database: process.env.SUBSCRIPTION_DB_NAME || 'subscription',
+});

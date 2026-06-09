e38191a15a9d468e0adc6e26f735ec6ba071dd06
DMoises <3033997670108@ingenieria.usac.edu.gt>
2026-06-08 23:43:31 -0600

    fix: provide fallback url in next config to prevent build failure

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -8,14 +8,15 @@ const nextConfig = {
     unoptimized: true,
   },
   async rewrites() {
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://api-gateway:8080';
     return [
       {
         source: '/api/auth/:path*',
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/auth/:path*`,
+        destination: `${apiUrl}/auth/:path*`,
       },
       {
         source: '/api/profiles/:path*',
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/auth/profiles/:path*`,
+        destination: `${apiUrl}/auth/profiles/:path*`,
       },
     ];
   },

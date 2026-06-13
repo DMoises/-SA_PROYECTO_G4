a6b4ec1faba974fe37218e73b7111137f58b1260
Juanjo264 <juanjoalmengor100@gmail.com>
2026-06-12 22:13:51 -0600

    feat: actualizacion base de datos catalogo

--- database/catalog/04_triggers.sql
+++ database/catalog/04_triggers.sql
@@ -1,3 +1,4 @@
 CREATE TRIGGER trg_validar_temporada
-    BEFORE INSERT ON temporadas
-    FOR EACH ROW EXECUTE FUNCTION trg_fn_validar_temporada();
+BEFORE INSERT OR UPDATE ON temporadas
+FOR EACH ROW
+EXECUTE FUNCTION trg_fn_validar_temporada();
